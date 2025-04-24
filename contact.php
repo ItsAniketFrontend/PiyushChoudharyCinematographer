3072edbdffdccae9b64a4e1d5fdb9230e27adad8
Aniket7726 <aniketsharma.sh17@gmail.com>
2025-04-24 11:56:24 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,281 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Us | Mitesh Khatri Photography</title>
+    <meta name="description" content="Contact or book the best photographer in Jaipur today. MK Photography is just a call away from you! Contact now to Book Us!">
+    <meta property="og:title" content="Mitesh Khatri Photography | Jaipur">
+<meta property="og:site_name" content="Mitesh Khatri Photography ">
+<meta property="og:url" content="https://miteshkhatriphotography.com/">
+<meta property="og:description" content="">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://miteshkhatriphotography.com/Images/about.jpg">
+    <link rel="icon" type="image/x-icon" href="Images/logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body oncontextmenu="return false">
+
+
+
+    <div class="sticky-btns">
+        <div class="d-flex flex-column">
+            <a href="tel: 7203861903"><img src="Images/phone-call.png" width="48" height="48" alt=""></a>
+            <a href="https://wa.me/7203861903"><img src="Images/whatsapp.png" width="48" height="48" alt=""></a>
+        </div>
+    </div>
+
+    <header>
+        <nav class="navbar navbar-expand-lg" id="navbar-example2">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="Images/logo-black.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" aria-current="page" href="index.php#scrollspyhome">Home</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="index.php#scrollspyabout">About</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="index.php#scrollspyportfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item mx-3">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section class="form py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-12">
+                        <h1>LET'S TALK</h1>
+                        <hr>
+                        <h5>Email</h5>
+                        <a href="tel: 7203861903">miteshkhatri720@gmail.com</a>
+                        <hr>
+                        <h5>Call</h5>
+                        <a href="tel: 7203861903"> +91-7203861903</a>
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <h2>Get in Touch</h2>
+                        <form action="">
+                            <div class="row">
+                                <div class="col-12 pb-2">
+                                    <input type="text" placeholder="Enter Your First Name">
+                                </div>
+                                <div class="col-12 pb-2">
+                                    <input type="text" placeholder="Enter Your Last Name">
+                                </div>
+                                <div class="col-12 pb-2">
+                                    <input type="email" placeholder="Enter Your Email">
+                                </div>
+                                <div class="col-12 pb-2">
+                                    <input type="number" placeholder="Phone">
+                                </div>
+                                <div class="col-12 pb-2">
+                                    <textarea name="" id="" placeholder="Enter Message" rows="5"></textarea>
+                                </div>
+                                <div class="col-12 pb-2">
+                                    <button>SUBMIT</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- <section class="instagram py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>LET’S MAKE SOME MEMORIES TOGETHER</h1>
+                        <p>STOP BY AND SAY ‘HI’</p>
+                        <div class="d-flex justify-content-center">
+                            <hr>
+                        </div>
+                        <h3>FOLLOW US ON INSTAGRAM</h3>
+                        <div class="d-flex justify-content-center pb-2">
+                            <a href="https://www.instagram.com/mitesh.khatri_?igsh=MW5kNW9semFtbGc4Mg=="><img src="Images/instagram.png" alt=""></a>
+                        </div>
+                        <div class="wrapper">
+                            <div class="gallery">
+                              <div class="image"><span><img src="Images/1-Wedding/1.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/1-Wedding/10.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/2-Actor/1 Darshan Raval/DR-1.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/5-Resto-Cafes-Hotels-Food/10.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/5-Resto-Cafes-Hotels-Food/12.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/4-Influencers/2 Aditi Mistry/1.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/5-Resto-Cafes-Hotels-Food/12.jpg" alt="" oncontextmenu="return false"></span></div>
+                              <div class="image"><span><img src="Images/4-Influencers/2 Aditi Mistry/1.jpg" alt="" oncontextmenu="return false"></span></div>
+                            </div>
+                        </div>
+                        <div class="preview-box">
+                            <div class="details">
+                              <span class="title">Image <p class="current-img"></p> of <p class="total-img"></p></span>
+                              <span class="icon fas fa-times"></span>
+                            </div>
+                            <div class="image-box">
+                              <div class="slide prev"><i class="fas fa-angle-left"></i></div>
+                              <div class="slide next"><i class="fas fa-angle-right"></i></div>
+                              <img src="" alt="">
+                            </div>
+                        </div>
+                        <div class="shadow"></div>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center mt-2">
+                        <button><a href="tel: 7203861903">BOOK US</a></button>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+    </main>
+
+    <footer>
+        <div class="sub-foot py-4">
+            <div class="container-fluid">
+                <h2 class="pb-3">Post & Blogs</h2>
+                <div class="logos">
+                    <div class="logos-slide">
+                        <img src="Images/1-Wedding/10.jpg" oncontextmenu="return false" />
+                        <img src="Images/1-Wedding/5.jpg" oncontextmenu="return false" />
+                        <img src="Images/4-Influencers/2 Aditi Mistry/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/5-Resto-Cafes-Hotels-Food/11.jpg" oncontextmenu="return false" />
+                        <img src="Images/4-Influencers/4 Parakram Rana/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/1-Wedding/3.jpg" oncontextmenu="return false" />
+                        <img src="Images/2-Actor/5 Divyanshu Kacholia/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/3-Pre-Wedding/0-32.jpg" oncontextmenu="return false" />
+                    </div>
+
+                    <div class="logos-slide">
+                        <img src="Images/1-Wedding/10.jpg" oncontextmenu="return false" />
+                        <img src="Images/1-Wedding/5.jpg" oncontextmenu="return false" />
+                        <img src="Images/4-Influencers/2 Aditi Mistry/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/5-Resto-Cafes-Hotels-Food/11.jpg" oncontextmenu="return false" />
+                        <img src="Images/4-Influencers/4 Parakram Rana/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/1-Wedding/3.jpg" oncontextmenu="return false" />
+                        <img src="Images/2-Actor/5 Divyanshu Kacholia/2.jpg" oncontextmenu="return false" />
+                        <img src="Images/3-Pre-Wedding/0-32.jpg" oncontextmenu="return false" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="foot">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-12 my-3">
+                        <span class="">
+                            <img src="Images/logo_2.png" height="150px" alt="">
+                            <p>Look and think before opening the shutter. The heart and mind are the true lens of the camera</p>
+                            <h5>~Mitesh Khatri</h5>
+                        </span>
+                    </div>
+                    <div class="col-lg-4 col-12 my-3">
+                        <h2>Contacts</h2>
+                        <p><span>Phone:</span><a href="tel: 7203861903"> +91-7203861903</a></p>
+                        <p><span>Email:</span><a href="mailto:">miteshkhatri720@gmail.com</a></p>
+                        <!-- <p><span>Address:</span>Mansarovar, Jaipur</p> -->
+                    </div>
+                    <div class="col-lg-4 col-12 my-3">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14236.453976631086!2d75.7591769!3d26.868135!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5a00160499b%3A0x5703b59fa53c7f09!2sMitesh%20Khatri%20Photography!5e0!3m2!1sen!2sin!4v1709139532220!5m2!1sen!2sin"
+                            width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="lower-foot">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <p>Copyright © 2024 MiteshKhatri Photographer. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+
+
+
+
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+        let slideIndex = 0;
+        showSlide(slideIndex);
+
+        function nextSlide() {
+            showSlide(slideIndex + 1);
+        }
+
+        function prevSlide() {
+            showSlide(slideIndex - 1);
+        }
+
+        function showSlide(n) {
+            const slides = document.getElementsByClassName("slide");
+            if (n >= slides.length) {
+                slideIndex = 0;
+            } else if (n < 0) {
+                slideIndex = slides.length - 1;
+            } else {
+                slideIndex = n;
+            }
+
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].classList.remove("active");
+            }
+
+            slides[slideIndex].classList.add("active");
+        }
+
+    </script>
+    <script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  "name": "Mitesh Khatri",
+  "url": "https://miteshkhatriphotography.com/",
+  "image": "https://miteshkhatriphotography.com/Images/about.jpg",
+  "sameAs": "https://www.instagram.com/mitesh.khatri_/",
+  "jobTitle": "Photographer in Jaipur",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Mitesh Khatri Photography"
+  }  
+}
+</script>
+    <script>
+        document.addEventListener('contextmenu', function(e) {
+          e.preventDefault();
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
